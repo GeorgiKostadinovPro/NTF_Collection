7e0ed2879609ced36f5ecdf615fe4af2940bddb6
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-10-13 16:43:29 +0300

    Add ERC721 standard to CoreNft

--- src/CoreNft.sol
+++ src/CoreNft.sol
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-
 pragma solidity ^0.8.18;
 
-contract CoreNft {}
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract CoreNft is ERC721 {
+    constructor() ERC721("DogNft", "DG") {}
+}
