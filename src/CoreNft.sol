61a21215e738b621afbe61187b3da92fcb913ca0
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-10-13 20:06:37 +0300

    Add unit tests

--- src/CoreNft.sol
+++ src/CoreNft.sol
@@ -7,14 +7,11 @@ contract CoreNft is ERC721 {
     uint256 private s_count;
     mapping(uint256 => string) private s_tokenIdToUri;
 
-    string private constant ipfsNft =
-        "ipfs://QmPX6RjAP33XSzfw9xfCcwrEFYZJu4qY3eZVKcesZW9bXP/?filename=dog-nft.json";
-
-    constructor() ERC721("DogNft", "DG") {
+    constructor() ERC721("Dogo", "DOG") {
         s_count = 0;
     }
 
-    function minNft(string memory _tokenUri) public {
+    function mintNft(string memory _tokenUri) public {
         s_tokenIdToUri[s_count] = _tokenUri;
         _safeMint(msg.sender, s_count);
         s_count++;
